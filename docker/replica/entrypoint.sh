d262af4702120226c13e305014174279ab702425
loremercer <hugopochta@gmail.com>
2024-06-03 13:34:37 +0300

    fix

--- docker/replica/entrypoint.sh
+++ docker/replica/entrypoint.sh
@@ -11,6 +11,7 @@ rm -rf /var/lib/postgresql/15/main
 pg_basebackup -h postgres -p 5432 -U otus -D /var/lib/postgresql/15/main -R
 echo 'port = 5433' >> /var/lib/postgresql/15/main/postgresql.auto.conf
 echo 'hot_standby = on' >> /var/lib/postgresql/15/main/postgresql.auto.conf
+echo "primary_slot_name = 'main'" >> /var/lib/postgresql/15/main/postgresql.auto.conf
 echo "primary_conninfo = 'host=$MASTER_HOST port=5432 user=$POSTGRES_USER password=$POSTGRES_PASSWORD'" >> /var/lib/postgresql/15/main/postgresql.auto.conf
 echo "recovery_min_apply_delay = '5min'" >> /var/lib/postgresql/15/main/postgresql.auto.conf
 pg_ctlcluster 15 main start
